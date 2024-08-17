d6f0aa4427e9059a27331abaad1689ef6667230a
Skrodzh <zenit.nadir@mail.ru>
2024-08-17 17:11:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+print('1st program')
+print(9**0.5*5)
+print('2nd program')
+print(9.99 > 8.98, 1000 != 1000.1)
+print('3rd program')
+print(2 * 2 + 2)
+print(2 * (2 +2))
+print((2 * 2) == (2 * (2+2)))
+print('4th program')
+string = '123.456'
+print(string[4])
+a = (float('123.456'))
+print(a)
+b = (a * 10)
+print(b)
+
+
+
+
+
