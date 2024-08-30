f02a42d3b86cf5d60c4cdd5587fb8ea6cc1f015c
Skrodzh <zenit.nadir@mail.ru>
2024-08-30 18:26:12 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-print('1st program')
-print(9**0.5*5)
-print('2nd program')
-print(9.99 > 8.98, 1000 != 1000.1)
-print('3rd program')
+'1st program'
+a = 9**0.5*5
+print(a)
+'2nd program'
+print(9.99 > 8.98 and 1000 != 1000.1)
+'3rd program'
 print(2 * 2 + 2)
 print(2 * (2 +2))
 print((2 * 2) == (2 * (2+2)))
-print('4th program')
+'4th program'
 string = '123.456'
 print(string[4])
 a = (float('123.456'))
 print(a)
 b = (a * 10)
 print(b)
+print(int(b))
+print(int(b % 10))
 
 
 
